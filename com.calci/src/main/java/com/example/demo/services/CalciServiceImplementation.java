83aa03f7d44a29e4d6b8ce3e806d9b0f31b11a1c
unnati kute <unnatikute777@gmail.com>
2025-11-19 00:23:50 +0530

    Initial Commit

---
+++ com.calci/src/main/java/com/example/demo/services/CalciServiceImplementation.java
@@ -0,0 +1,35 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalciServiceImplementation 
+implements CalciServices{
+
+	@Override
+	public int add(int num1, int num2) {
+		
+		return num1 + num2;
+	}
+
+	@Override
+	public int sub(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 - num2;
+	}
+
+	@Override
+	public int mul(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 * num2;
+	}
+
+	@Override
+	public int div(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 / num2;
+	}
+	
+}
+	
+
